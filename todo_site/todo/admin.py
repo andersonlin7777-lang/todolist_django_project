2aa31e06523332c193ea39c902dfcca0d8305df6
andersonlin7777-lang <andersonlin7777@gmail.com>
2026-01-13 15:35:14 +0800

    add forms.py and modify admin.py

--- todo_site/todo/admin.py
+++ todo_site/todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+#Registering the model allows you to manage to-do items via Django’s built-in admin interface.
+admin.site.register(Todo)
+
---
+++ todo_site/todo/forms.py
@@ -0,0 +1,14 @@
+#從 Django 倉庫裡拿出「表單工具箱」
+from django import forms
+#告訴 Django：「我要根據我們之前在同一個資料夾下定義好的 Todo 模型來製作表單」
+from .models import Todo
+#在 Django 中，TodoForm 繼承自 ModelForm，它的任務是幫你把資料庫的模型（Model）轉化成網頁表單
+#TodoForm（本體）：代表這個表單**「是什麼」以及它具備什麼「功能」**
+class TodoForm(forms.ModelForm):
+    #class Meta（設定）：代表這個表單**「如何與外界連結」。
+    # 它像是一個「參數面板」**，用來告訴 Django 引擎如何去讀取你的設計圖
+    class Meta:
+        model = Todo
+        #「把 Todo 模型裡的所有欄位（title、details、date）通通都放到表單上，讓使用者可以填寫」
+        fields = "__all__"
+        
